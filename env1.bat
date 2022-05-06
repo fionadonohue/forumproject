b8956384595748f32eb981d06702b61d6a9a1a8f
Yharai <yharai5cm@gmail.com>
2022-05-06 09:54:54 -0700

    update

--- env1.bat
+++ env1.bat
@@ -1,1 +1,6 @@
+set GITHUB_CLIENT_ID=d28cba4c63d9fc983f52   
+set GITHUB_CLIENT_SECRET=e739d7f3886333cb91497af0db5de48f2a3c6dee
+set APP_SECRET_KEY=abcd1234
 
+set MONGO_CONNECTION_STRING=mongodb+srv://forum-project:<password>@forum-cluster.tejjs.mongodb.net/myFirstDatabase?retryWrites=true
+set MONGO_DBNAME=forumproject
--- webapp.py
+++ webapp.py
@@ -1,41 +1,39 @@
 from flask import Flask, redirect, url_for, session, request, jsonify
-from flask_oauthlib.client import OAuth
+#from flask_oauthlib.client import OAuth
 from flask import render_template
 
 import pprint
 import os
 
-# This code originally from https://github.com/lepture/flask-oauthlib/blob/master/example/github.py
-# Edited by P. Conrad for SPIS 2016 to add getting Client Id and Secret from
-# environment variables, so that this will work on Heroku.
-# Edited by S. Adams for Designing Software for the Web to add comments and remove flash messaging
-
 app = Flask(__name__)
 
 app.debug = False #Change this to False for production
-#os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #Remove once done debugging
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1' #Remove once done debugging
 
-app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
-oauth = OAuth(app)
-oauth.init_app(app) #initialize the app to be able to make requests for user information
+#app.secret_key = os.environ['SECRET_KEY'] #used to sign session cookies
+#oauth = OAuth(app)
+#oauth.init_app(app) #initialize the app to be able to make requests for user information
 
-#Set up GitHub as OAuth provider
-github = oauth.remote_app(
-    'github',
-    consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
-    consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
-    request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
-    base_url='https://api.github.com/',
-    request_token_url=None,
-    access_token_method='POST',
-    access_token_url='https://github.com/login/oauth/access_token',  
-    authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
-)
+#github = oauth.remote_app(
+    #'github',
+    #consumer_key=os.environ['GITHUB_CLIENT_ID'], #your web app's "username" for github's OAuth
+    #consumer_secret=os.environ['GITHUB_CLIENT_SECRET'],#your web app's "password" for github's OAuth
+    #request_token_params={'scope': 'user:email'}, #request read-only access to the user's email.  For a list of possible scopes, see developer.github.com/apps/building-oauth-apps/scopes-for-oauth-apps
+    #base_url='https://api.github.com/',
+    #request_token_url=None,
+    #access_token_method='POST',
+    #access_token_url='https://github.com/login/oauth/access_token',  
+    #authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
+#)
+
+connection_string = os.environ["MONGO_CONNECTION_STRING"]
+db_name = os.environ["MONGO_DBNAME"]
+    
+client = pymongo.MongoClient(connection_string)
+db = client[db_name]
+collection = db['data'] 
 
 
-#context processors run before templates are rendered and add variable(s) to the template's context
-#context processors must return a dictionary 
-#this context processor adds the variable logged_in to the conext for all templates
 @app.context_processor
 def inject_logged_in():
     return {"logged_in":('github_token' in session)}
@@ -44,7 +42,7 @@ def inject_logged_in():
 def home():
     return render_template('home.html')
 
-#redirect to GitHub's OAuth page and confirm callback URL
+    
 @app.route('/login')
 def login():   
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
@@ -64,8 +62,8 @@ def authorized():
         try:
             session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
             session['user_data']=github.get('user').data
-            #pprint.pprint(vars(github['/email']))
-            #pprint.pprint(vars(github['api/2/accounts/profile/']))
+            pprint.pprint(vars(github['/email']))
+            pprint.pprint(vars(github['api/2/accounts/profile/']))
             message='You were successfully logged in as ' + session['user_data']['login'] + '.'
         except Exception as inst:
             session.clear()
@@ -77,7 +75,6 @@ def authorized():
 @app.route('/page1')
 def renderPage1():
     return render-template('page1.html')
-if ("Kendall") or ("Kylie") = ("KENDALL & KYLIE")("app1")
     
     
 
@@ -86,9 +83,9 @@ def renderPage2():
     return render_template('page2.html')
 
 
-@github.tokengetter
-def get_github_oauth_token():
-    return session['github_token']
+#@github.tokengetter
+#def get_github_oauth_token():
+    #return session['github_token']
 
 
 if __name__ == '__main__':
